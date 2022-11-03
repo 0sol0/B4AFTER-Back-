2671675451a9bd8610b8c18424b4f0a1ab94b11f
정형빈 <gudqls369@gmail.com>
2022-11-03 20:49:48 +0900

    Feat db_uploader

--- books/models.py
+++ books/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    class Meta:
+        db_table = "book"
+        
+    title = models.CharField(max_length=1200, primary_key=True)
+    author = models.CharField(max_length=1000, null=True)
+    publisher = models.CharField(max_length=1000, null=True)
+    pub_date = models.CharField(max_length=10, null=True)
+    img_url = models.CharField(max_length=1000, null=True)
+    description = models.TextField(null=True)
+    isbn13 = models.CharField(max_length=13, null=True)
+    
+    def __str__(self):
+        return self.title
