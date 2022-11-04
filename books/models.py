4d23d52658c8aa7a7819f511690c8132d44f5da2
gudqls369 <gudqls369@gmail.com>
2022-11-04 16:55:55 +0900

    Fix DB and books.json include category

--- books/models.py
+++ books/models.py
@@ -13,6 +13,7 @@ class Book(models.Model):
     img_url = models.CharField(max_length=1000, null=True)
     description = models.TextField(null=True)
     isbn13 = models.CharField(max_length=13, null=True)
+    category = models.CharField(max_length=5, null=True)
     
     def __str__(self):
         return self.title
