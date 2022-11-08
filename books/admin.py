e7f2ca6d834f34daf58e377a78e9a2889b035efa
gudqls369 <gudqls369@gmail.com>
2022-11-08 11:58:10 +0900

    update requirements.txt and upload files

--- books/admin.py
+++ books/admin.py
@@ -1,14 +1,7 @@
 from django.contrib import admin
-<<<<<<< HEAD
-from .models import Book, Review
+from .models import Book, Review, Image
 
 # Register your models here.
 admin.site.register(Book)
 admin.site.register(Review)
-=======
-from .models import Book, Image
-
-# Register your models here.
-admin.site.register(Book)
 admin.site.register(Image)
->>>>>>> c48b0508404cc1fc400a4fe333a70e542af33595
